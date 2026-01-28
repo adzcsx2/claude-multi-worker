e1a16681a55eae3d5a4e40988b7b68e72d7c5af9
hoyn <adzcsx2@gmail.com>
2026-01-28 18:39:21 +0800

    文档: 更新README，添加默认实例使用说明

---
+++ .cms_config/temp_send.ps1
@@ -0,0 +1,10 @@
+
+$Host.UI.RawUI.WindowTitle = "Sending to c1"
+# Focus the target tab and send text
+wt.exe -w 0 focus-tab --target 0
+Start-Sleep -Milliseconds 500
+
+# Send the text character by character (workaround for WT limitation)
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.SendKeys]::SendWait("hello")
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
